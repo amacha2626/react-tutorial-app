a1e83c7a71218cdfdc4da116e995798864de1c18
amacha2626 <amacha2626@gmail.com>
2020-08-01 01:01:09 +0900

    add title component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world
-        </p>
+        <Title title="Hello, World 3.0" />
         <a
           className="App-link"
           href="https://reactjs.org"
