9328dc2fa54be1aa31f351679d3a17e90dc67397
amacha2626 <amacha2626@gmail.com>
2020-08-01 01:21:33 +0900

    add changetext button and learn props children

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title title="Hello, World 3.0" titleStyle={{color: '#56D6FB'}} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  onClickHandler = () => {
+    let title = document.getElementById('versionStatement')
+    let upgradeButton = document.getElementById('upgradeButton')
+    title.textContent = "Hello World 4.0"
+    upgradeButton.style.display = "none"
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Title
+            title="Hello, World 3.0"
+            titleStyle={{color: '#56D6FB'}} 
+            onClick={this.onClickHandler}
+          >
+            Hello World <span id="versionCounter" style={{ borderBottom: '1px solid orange'}}>3.0</span>
+          </Title>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Title = (props) => {
   return(
     <div>
-      <h2 style={ props.titleStyle }>{ props.title }</h2>
+      <h2
+        style={ props.titleStyle }
+        id="versionStatement"
+      >{ props.children }</h2>
+      <p
+        onClick={ props.onClick }
+        id="upgradeButton"
+      >Upgrade</p>
     </div>
   )
 }
